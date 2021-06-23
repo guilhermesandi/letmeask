cac6b6a7101ddeb12b9e8dc5c0b948706dba5d83
Guilherme Sandi <guilherme_sandi@hotmail.com>
2021-06-23 01:52:44 -0300

    Create useAuth

--- src/pages/NewRoom.tsx
+++ src/pages/NewRoom.tsx
@@ -1,16 +1,15 @@
-// import { useContext } from 'react';
 import { Link } from 'react-router-dom';
-// import { AuthContext } from '../contexts/AuthContext';
 
 import illustrationImg from '../assets/images/illustration.svg';
 import logoImg from '../assets/images/logo.svg';
 
 import { Button } from '../components/Button';
+// import { useAuth } from '../hooks/useAuth';
 
 import '../styles/auth.scss';
 
 export function NewRoom() {
-  // const { user } = useContext(AuthContext);
+  // const { user } = useAuth();
 
   return (
     <div id="page-auth">
