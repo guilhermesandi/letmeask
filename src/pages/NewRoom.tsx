ca533abc227a0324a8361d40dbee8c359c65aafa
Guilherme Sandi <guilherme_sandi@hotmail.com>
2021-06-24 02:04:18 -0300

    Finish room creation

--- src/pages/NewRoom.tsx
+++ src/pages/NewRoom.tsx
@@ -1,5 +1,5 @@
 import { FormEvent, useState } from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useHistory } from 'react-router-dom';
 
 import illustrationImg from '../assets/images/illustration.svg';
 import logoImg from '../assets/images/logo.svg';
@@ -12,7 +12,7 @@ import '../styles/auth.scss';
 
 export function NewRoom() {
   const { user } = useAuth();
-
+  const history = useHistory();
   const [newRoom, setNewRoom] = useState('');
 
   async function handleCreateRoom(event: FormEvent) {
@@ -27,7 +27,9 @@ export function NewRoom() {
     const firebaseRoom = await roomRef.push({
       title: newRoom,
       authorId: user?.id,
-    })
+    });
+
+    history.push(`/rooms/${firebaseRoom.key}`);
   }
 
   return (
