30dd0617b2875dd7a944a8e89353cd9493a8e3d7
Guilherme Sandi <guilherme_sandi@hotmail.com>
2021-06-24 20:19:21 -0300

    Listening questions

--- src/pages/Room.tsx
+++ src/pages/Room.tsx
@@ -47,7 +47,7 @@ export function Room() {
   useEffect(() => {
     const roomRef = database.ref(`rooms/${roomId}`);
 
-    roomRef.once('value', room => {
+    roomRef.on('value', room => {
       const databaseRoom = room.val();
       const firebaseQuestions: FirebaseQuestions = databaseRoom.questions ?? {};
 
